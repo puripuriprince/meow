b394b60670353babb75182fdc8351d549b04747b
Katherine-lambert <k.lambertcanada@gmail.com>
2025-09-13 20:16:42 -0400

    Created tools directory

---
+++ tools/adapters/setup.sh
@@ -0,0 +1,8 @@
+# Must have these tools installed inside the system
+
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/ffuf/ffuf/v2@latest
+go install github.com/projectdiscovery/httpx/cmd/httpx@latest
+npm install -g wappalyzer
+go install github.com/hakluke/hakrawler@latest
+
